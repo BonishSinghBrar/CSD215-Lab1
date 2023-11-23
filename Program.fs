b46a4a4181afe601b7895637387dfc5457183c9d
Bonish_Singh <bonishsinghbrar@gmail.com>
2023-11-23 10:10:06 -0500

    v2

--- Program.fs
+++ Program.fs
@@ -8,3 +8,9 @@ type team = {
     Name: string
     Coach: Coach
     stats: stats}
+let team = [
+   {Name="Chicago Bulls", Coach = {Name="Billy Donovan"; formerplayer=true};Stats={ Wins= 2344;Losses= 2254 }}
+   {Name="Milwaukee Bucks", Coach = {Name="Adrian Griffin"; formerplayer=false};Stats={ Wins= 2340;Losses= 2103 }}
+   {Name="Chicago Bulls", Coach = {Name="Billy Donovan"; formerplayer=true};Stats={ Wins= 2344;Losses= 2254}}
+   {Name="Chicago Bulls", Coach = {Name="Billy Donovan"; formerplayer=true};Stats={ Wins= 2344;Losses= 2254}}
+   {Name="Chicago Bulls", Coach = {Name="Billy Donovan"; formerplayer=true};Stats={ Wins= 2344;Losses= 2254}}]
